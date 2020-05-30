4ff6b075a09ae33068bd1befd3edcc4b8ad5c122
Bimo Adityarahman Wiraputra <bimo.awm1@gmail.com>
2020-05-31 00:56:43 +0700

    Move api specific error handling to api folder

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,11 +1,25 @@
-import { Router } from 'express';
+import { Router, Request, Response, NextFunction } from 'express';
 
 import defaultRoute from './routes/default';
 import divisionRoute from './routes/division';
+import ParsedError from '../helpers/parsed-error';
+import sendResponse from './send-response';
 
 const route: Router = Router();
 
 route.use('/divisions', divisionRoute);
 route.use(defaultRoute);
+
+route.use((err: Error, _req: Request, res: Response, next: NextFunction) => {
+  if (err) {
+    if (err instanceof ParsedError) {
+      sendResponse(res, err.code, err.data || err.message);
+    } else {
+      sendResponse(res, 500, err.message);
+    }
+  } else {
+    next();
+  }
+});
 
 export default route;
--- src/loaders/express.ts
+++ src/loaders/express.ts
@@ -6,28 +6,21 @@ import cors from 'cors';
 
 import config from '../config';
 import route from '../api';
-import ParsedError from '../helpers/parsed-error';
 import sendResponse from '../api/send-response';
 
 const app: Application = express();
 app.enable('trust proxy');
 app.use(cors());
 app.use(bodyParser.json());
-app.use(config.api.prefix, route);
-
 app.use((err: Error, _req: Request, res: Response, next: NextFunction) => {
-  if (err) {
-    if (err instanceof ParsedError) {
-      sendResponse(res, err.code, err.data || err.message);
-    } else if (err instanceof SyntaxError) {
-      sendResponse(res, 400, { body: 'body is badly formatted' });
-    } else {
-      sendResponse(res, 500, err.message);
-    }
+  if (err instanceof SyntaxError) {
+    sendResponse(res, 400, { body: 'body is badly formatted' });
   } else {
     next();
   }
 });
+
+app.use(config.api.prefix, route);
 
 app.use((_req, res) => {
   sendResponse(res, 404, { path: "method requested doesn't exist" });
